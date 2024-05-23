2e8c96a17d1f5a949444c33b2779c029ac8f7987
meow <coffe1n21@gmail.com>
2024-05-23 07:59:44 +0700

    22.05.2024

--- src/ComparisonTable.tsx
+++ src/ComparisonTable.tsx
@@ -1,17 +1,20 @@
 import React, { useState } from 'react';
-import Modal from './Modal';
-import phonesData, { Phone } from './PhonesData';
+import Modal from './Modal'; // Импорт компонента модального окна
+import phonesData, { Phone } from './PhonesData'; // Импорт данных о телефонах
 
 const ComparisonTable: React.FC = () => {
-  const [phones, setPhones] = useState<Phone[]>(phonesData.slice(0, 2));
+  // Состояния компонента: список телефонов, открыто ли модальное окно, индекс текущего столбца
+  const [phones, setPhones] = useState<Phone[]>(phonesData.slice(0, 3));
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [currentColumnIndex, setCurrentColumnIndex] = useState<number | null>(null);
 
+  // Функция для открытия модального окна и сохранения индекса текущего столбца
   const openModal = (index: number): void => {
     setCurrentColumnIndex(index);
     setIsModalOpen(true);
   };
 
+  // Функция для выбора телефона в модальном окне и обновления списка телефонов
   const selectPhone = (phone: Phone): void => {
     if (currentColumnIndex !== null) {
       const updatedPhones = [...phones];
@@ -21,48 +24,113 @@ const ComparisonTable: React.FC = () => {
     setIsModalOpen(false);
   };
 
+  // Функция для изменения количества столбцов в таблице
   const changeColumns = (numColumns: number): void => {
     setPhones(phonesData.slice(0, numColumns));
   };
 
+  // Функция для отображения иконок на основе булевых значений
+  const renderBooleanIcon = (value: boolean): JSX.Element => {
+    const iconSrc = value ? 'src/assets/ic_true.svg' : 'src/assets/ic_false.svg';
+    return <img src={iconSrc} alt={value ? 'Да' : 'Нет'} style={{ height: '20px' }} />;
+  };
+
   return (
     <div>
+      {/* Группа кнопок для выбора количества столбцов в таблице */}
       <div className="button-group">
         {[2, 3, 4, 5, 6].map((num) => (
           <button key={num} onClick={() => changeColumns(num)}>
-            {num} Columns
+            {num}
           </button>
         ))}
       </div>
+      {/* Таблица для сравнения характеристик телефонов */}
       <table>
         <thead>
           <tr>
+            <th>
+              <input type="checkbox" name="display"/>
+              <label>Показать различия</label>
+            </th>
             {phones.map((phone, index) => (
               <th key={index}>
-                <button onClick={() => openModal(index)}>Change</button>
+                {/* Кнопка для открытия модального окна с выбором телефона */}
+                <button onClick={() => openModal(index)}>Изменить</button>
+                {/* Изображение и название телефона */}
+                <div>
+                  <img src={phone.image} alt={phone.name} style={{ height: '120px' }} />
+                </div>
                 {phone.name}
               </th>
             ))}
           </tr>
         </thead>
         <tbody>
+          {/* Тело таблицы с характеристиками телефонов */}
           <tr>
+            <td>ПРОИЗВОДИТЕЛЬ</td>
             {phones.map((phone, index) => (
               <td key={index}>{phone.feature1}</td>
             ))}
           </tr>
           <tr>
+            <td>ГОД РЕЛИЗА</td>
             {phones.map((phone, index) => (
               <td key={index}>{phone.feature2}</td>
             ))}
           </tr>
           <tr>
+            <td>ДИАГОНАЛЬ ЭКРАНА (ДЮЙМ)</td>
             {phones.map((phone, index) => (
               <td key={index}>{phone.feature3}</td>
             ))}
           </tr>
+          <tr>
+            <td>СТРАНА-ПРОИЗВОДИТЕЛЬ</td>
+            {phones.map((phone, index) => (
+              <td key={index}>{phone.feature4}</td>
+            ))}
+          </tr>
+          <tr>
+            <td>ОБЪЕМ ПАМЯТИ</td>
+            {phones.map((phone, index) => (
+              <td key={index}>{phone.feature5}</td>
+            ))}
+          </tr>
+          <tr>
+            <td>ЧАСТОТА ОБНОВЛЕНИЯ ЭКРАНА</td>
+            {phones.map((phone, index) => (
+              <td key={index}>{phone.feature6}</td>
+            ))}
+          </tr>
+          <tr>
+            <td>NFC</td>
+            {phones.map((phone, index) => (
+              <td key={index}>{renderBooleanIcon(phone.feature7)}</td>
+            ))}
+          </tr>
+          <tr>
+            <td>ПОДДЕРЖКА ESIM</td>
+            {phones.map((phone, index) => (
+              <td key={index}>{renderBooleanIcon(phone.feature8)}</td>
+            ))}
+          </tr>
+          <tr>
+            <td>ПОДДЕРЖКА БЕСПРОВОДНОЙ ЗАРЯДКИ</td>
+            {phones.map((phone, index) => (
+              <td key={index}>{renderBooleanIcon(phone.feature9)}</td>
+            ))}
+          </tr>
+          <tr>
+            <td>СТОИМОСТЬ</td>
+            {phones.map((phone, index) => (
+              <td key={index}>{phone.feature10}</td>
+            ))}
+          </tr>
         </tbody>
       </table>
+      {/* Модальное окно для выбора телефона */}
       {isModalOpen && <Modal phones={phonesData} selectPhone={selectPhone} />}
     </div>
   );
--- src/PhonesData.tsx
+++ src/PhonesData.tsx
@@ -1,19 +1,104 @@
 export interface Phone {
-    name: string;
-    feature1: string;
-    feature2: string;
-    feature3: string;
+  name: string;
+  image: string;
+  feature1: string; // Производитель
+  feature2: string; // Год релиза
+  feature3: string; // Диагональ экрана (дюйм)
+  feature4: string; // Страна-производитель
+  feature5: string; // Объем памяти
+  feature6: string; // Частота обновления экрана
+  feature7: boolean; // NFC
+  feature8: boolean; // Поддержка eSIM
+  feature9: boolean; // Поддержка беспроводной зарядки
+  feature10: string; // Стоимость
+}
+
+// Обновленные данные телефонов
+const PhonesData: Phone[] = [
+  {
+    name: 'Apple Iphone 12',
+    image: 'src/assets/Apple_iphone_12.png',
+    feature1: 'Apple', 
+    feature2: '2020', 
+    feature3: '6.1', 
+    feature4: 'Китай', 
+    feature5: '64 ГБ / 128 ГБ / 256 ГБ', 
+    feature6: '60 Гц', 
+    feature7: true,
+    feature8: false,
+    feature9: false,
+    feature10: '$799' 
+  },
+  {
+    name: 'Xiaomi Mi 11 Lite',
+    image: 'src/assets/Xiaomi_Mi_11_Lite.png',
+    feature1: 'Xiaomi',
+    feature2: '2021',
+    feature3: '6.55',
+    feature4: 'Китай',
+    feature5: '64 ГБ / 128 ГБ',
+    feature6: '90 Гц',
+    feature7: true,
+    feature8: false,
+    feature9: false,
+    feature10: '$299'
+  },
+  {
+    name: 'Samsung Galaxy A72',
+    image: 'src/assets/samsing_a72.png',
+    feature1: 'Samsung',
+    feature2: '2021',
+    feature3: '6.7',
+    feature4: 'Вьетнам',
+    feature5: '128 ГБ / 256 ГБ',
+    feature6: '90 Гц',
+    feature7: true,
+    feature8: false,
+    feature9: false,
+    feature10: '$449'
+  },
+  {
+    name: 'Samsung Galaxy S21',
+    image: 'src/assets/samsung_gelaxy_s_21.png',
+    feature1: 'Samsung', 
+    feature2: '2021', 
+    feature3: '6.2',
+    feature4: 'Южная Корея',
+    feature5: '128 ГБ / 256 ГБ',
+    feature6: '120 Гц',
+    feature7: true,
+    feature8: true,
+    feature9: true,
+    feature10: '$799'
+  },
+  {
+    name: 'Apple iPhone Xr',
+    image: 'src/assets/apple_iphone_xr.png',
+    feature1: 'Apple',
+    feature2: '2018',
+    feature3: '6.1',
+    feature4: 'Китай',
+    feature5: '64 ГБ / 128 ГБ / 256 ГБ',
+    feature6: '60 Гц',
+    feature7: true,
+    feature8: false,
+    feature9: false,
+    feature10: '$499'
+  },
+  {
+    name: 'Realme 8 Pro',
+    image: 'src/assets/realme_8_pro.png',
+    feature1: 'Realme',
+    feature2: '2024',
+    feature3: '6.4',
+    feature4: 'Китай',
+    feature5: '128 ГБ',
+    feature6: '60 Гц', 
+    feature7: true,
+    feature8: false,
+    feature9: true,
+    feature10: '$329'
   }
-  
-  const PhonesData: Phone[] = [
-    { name: 'Phone 1', feature1: 'Feature 1-1', feature2: 'Feature 1-2', feature3: 'Feature 1-3' },
-    { name: 'Phone 2', feature1: 'Feature 2-1', feature2: 'Feature 2-2', feature3: 'Feature 2-3' },
-    { name: 'Phone 3', feature1: 'Feature 3-1', feature2: 'Feature 3-2', feature3: 'Feature 3-3' },
-    { name: 'Phone 4', feature1: 'Feature 4-1', feature2: 'Feature 4-2', feature3: 'Feature 4-3' },
-    { name: 'Phone 5', feature1: 'Feature 5-1', feature2: 'Feature 5-2', feature3: 'Feature 5-3' },
-    { name: 'Phone 6', feature1: 'Feature 6-1', feature2: 'Feature 6-2', feature3: 'Feature 6-3' },
-  ];
-  
-  export default PhonesData;
-  
-  
+];
+
+export default PhonesData;
